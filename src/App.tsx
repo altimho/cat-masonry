ba23d80a6e25ba482acf45e936125a6fdcf6be2e
Igor Karpinskiy <ik@altimho.com>
2025-04-20 18:05:36 +0400

    App component base.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <h1>Test Masonry App</h1>;
-}
+import { Outlet } from 'react-router';
 
-export default App;
+export const App = () => {
+  return (
+    <>
+      <h1>Test Masonry App</h1>
+      <div>
+        <a href="https://www.pexels.com">Photos provided by Pexels</a>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
