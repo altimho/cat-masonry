33d3748eae60ec1fa0a8330553892317837afdb9
Igor Karpinskiy <ik@altimho.com>
2025-04-21 01:22:44 +0400

    Base list styles.

--- src/PhotoList/PhotoList.tsx
+++ src/PhotoList/PhotoList.tsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from 'react-router';
+import { css } from '@emotion/react';
 
 import { type photoListLoader } from './loader';
 import { PhotoListItem } from './PhotoListItem';
@@ -7,11 +8,17 @@ interface PhotoListProps {
   className?: string;
 }
 
+const photoListCss = css({
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+});
+
 export const PhotoList = ({ className }: PhotoListProps) => {
   const { photoIds } = useLoaderData<typeof photoListLoader>();
 
   return (
-    <div className={className}>
+    <div css={photoListCss} className={className}>
       {photoIds.map((photoId) => (
         <PhotoListItem key={photoId} photoId={photoId} />
       ))}
