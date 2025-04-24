f2545ab8b36dec3a57ab148f0fa1826ab351fbe4
Igor Karpinskiy <ik@altimho.com>
2025-04-25 03:40:18 +0400

    Some styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 import { Outlet } from 'react-router';
+import { css, Global } from '@emotion/react';
+
+const globalCss = css({
+  body: {
+    margin: 0,
+    padding: 0,
+    backgroundColor: '#FFF1D5',
+    fontFamily: 'sans-serif',
+  },
+});
+
+const headCss = css({
+  backgroundColor: '#9FB3DF',
+  padding: '0.5rem 1.5rem',
+  margin: 0,
+  marginBottom: '4px',
+  color: '#FFF1D5',
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+  a: {
+    color: '#FFF1D5',
+  },
+});
+
+const titleCss = css({
+  fontSize: '5rem',
+  fontWeight: 'lighter',
+  margin: 0,
+  padding: 0,
+  textShadow: '0 0.25rem 0.5rem rgba(0,0,0,0.25)',
+  textWrap: 'nowrap',
+});
 
 export const App = () => {
   return (
     <>
-      <h1>Test Masonry App</h1>
-      <div>
-        <a href="https://www.pexels.com">Photos provided by Pexels</a>
+      <Global styles={globalCss} />
+      <div css={headCss}>
+        <h1 css={titleCss}>Mas😺nry</h1>
+        <div>
+          <a href="https://www.pexels.com">Photos provided by Pexels</a>
+        </div>
       </div>
       <div>
         <Outlet />
--- src/pages/PhotoList/PhotoListItem.tsx
+++ src/pages/PhotoList/PhotoListItem.tsx
@@ -15,8 +15,8 @@ const photoListItemWrappeerCss = css({
 const photoListItemCss = css({
   width: '100%',
   height: '100%',
-  border: '2px solid white',
-  margin: '-2px',
+  border: '4px solid #FFF1D5',
+  margin: '-4px',
   background: 'lightgray',
 });
 
